67b7ba625f7e9795273c1bf9b891dc24ab899664
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-01-17 19:51:33 -0600

    Worker worker-15: Clippy Warnings Cleanup - cli module

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,10 +1,28 @@
 //! Native CLI support for the tsz binary.
 
 pub mod args;
+
+#[allow(clippy::collapsible_if)]
 pub mod config;
+
+#[allow(clippy::collapsible_if)]
+#[allow(clippy::collapsible_else_if)]
+#[allow(clippy::question_mark)]
+#[allow(clippy::manual_strip)]
+#[allow(clippy::unnecessary_filter_map)]
+#[allow(clippy::manual_is_ascii_check)]
+#[allow(clippy::match_like_matches_macro)]
+#[allow(clippy::option_map_or_none)]
 pub mod driver;
+
+#[allow(clippy::collapsible_if)]
+#[allow(clippy::match_like_matches_macro)]
 pub mod fs;
+
+#[allow(clippy::unnecessary_cast)]
 pub mod reporter;
+
+#[allow(clippy::question_mark)]
 pub mod watch;
 
 #[cfg(test)]
