a83038e00c3911e88ae6cc5d8fa7707f55c0cd2a
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-02-04 14:38:02 +0100

    chore: remove test files

--- test_exhaustiveness.ts
+++
@@ -1,9 +0,0 @@
-type Action = { type: "add" } | { type: "remove" };
-function handle(action: Action) {
-  switch (action.type) {
-    case "add": break;
-    case "remove": break;
-    default:
-      const impossible: never = action;
-  }
-}
