22158c21856bfd1f9be59f690913ef1f2fa10470
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-03-26 08:10:38 +0100

    fix(checker): preserve generic jsx callback context

--- crates/tsz-checker/tests/jsx_component_attribute_tests.rs
+++ crates/tsz-checker/tests/jsx_component_attribute_tests.rs
@@ -1293,6 +1293,149 @@ declare function Test<C extends keyof Elements>(props: Props<C>): null;
 }
 
 #[test]
+fn test_generic_jsx_defaulted_props_contextually_type_callback_attrs() {
+    let source = format!(
+        r#"
+{JSX_PREAMBLE}
+type Tag = "a" | "button";
+type Props<T extends Tag = "a"> = {{
+    as?: T;
+}} & (T extends "a"
+    ? {{ onClick?: (e: {{ href: string }}) => void }}
+    : {{ onClick?: (e: {{ disabled: boolean }}) => void }});
+
+declare function UnwrappedLink<T extends Tag = "a">(props: Props<T>): null;
+
+<UnwrappedLink onClick={{(e) => e.href}} />;
+<UnwrappedLink as="button" onClick={{(e) => e.disabled}} />;
+"#
+    );
+
+    let diags = jsx_diagnostics(&source);
+    assert!(
+        !has_code(&diags, diagnostic_codes::PARAMETER_IMPLICITLY_HAS_AN_TYPE),
+        "defaulted generic JSX props should contextually type callback attrs, got: {diags:?}"
+    );
+    assert!(
+        !has_code(&diags, diagnostic_codes::TYPE_IS_NOT_ASSIGNABLE_TO_TYPE),
+        "defaulted generic JSX props should keep callback members assignable, got: {diags:?}"
+    );
+}
+
+#[test]
+fn test_generic_jsx_defaulted_conditional_props_contextually_type_callback_attrs() {
+    let source = format!(
+        r#"
+{JSX_PREAMBLE}
+type ElementType = "a" | "button";
+type Exclude<T, U> = T extends U ? never : T;
+type Pick<T, K extends keyof T> = {{ [P in K]: T[P] }};
+type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+type ComponentPropsWithRef<T extends ElementType> =
+    T extends "a"
+        ? {{ as?: never; onClick?: (e: {{ href: string }}) => void }}
+        : {{ as?: never; onClick?: (e: {{ disabled: boolean }}) => void }};
+
+declare function UnwrappedLink<T extends ElementType = ElementType>(
+    props: Omit<ComponentPropsWithRef<ElementType extends T ? "a" : T>, "as">,
+): null;
+
+declare function UnwrappedLink2<T extends ElementType = ElementType>(
+    props: Omit<ComponentPropsWithRef<ElementType extends T ? "a" : T>, "as"> & {{ as?: T }},
+): null;
+
+<UnwrappedLink onClick={{(e) => e.href}} />;
+<UnwrappedLink2 onClick={{(e) => e.href}} />;
+<UnwrappedLink2 as="button" onClick={{(e) => e.disabled}} />;
+"#
+    );
+
+    let diags = jsx_diagnostics(&source);
+    assert!(
+        !has_code(&diags, diagnostic_codes::PARAMETER_IMPLICITLY_HAS_AN_TYPE),
+        "defaulted conditional generic JSX props should contextually type callback attrs, got: {diags:?}"
+    );
+    assert!(
+        !has_code(&diags, diagnostic_codes::TYPE_IS_NOT_ASSIGNABLE_TO_TYPE),
+        "defaulted conditional generic JSX props should keep callback members assignable, got: {diags:?}"
+    );
+}
+
+#[test]
+fn test_generic_jsx_react_style_defaulted_conditional_props_contextually_type_callback_attrs() {
+    let source = format!(
+        r#"
+{JSX_PREAMBLE}
+declare namespace React {{
+    interface ReactElement<T = any> {{}}
+    interface Element {{}}
+    interface AnchorElement extends Element {{ href: string; }}
+    interface ButtonElement extends Element {{ disabled: boolean; }}
+    interface MouseEvent<T = Element> {{ currentTarget: T; }}
+    type EventHandler<E> = {{ bivarianceHack(event: E): void }}["bivarianceHack"];
+    type MouseEventHandler<T = Element> = EventHandler<MouseEvent<T>>;
+
+    interface JSXElementConstructor<P> {{
+        (props: P): ReactElement<any> | null;
+    }}
+    interface ComponentType<P = {{}}> {{
+        (props: P): ReactElement<any> | null;
+    }}
+    type ElementType<P = any> =
+        {{
+            [K in keyof JSX.IntrinsicElements]: P extends JSX.IntrinsicElements[K] ? K : never
+        }}[keyof JSX.IntrinsicElements]
+        | ComponentType<P>;
+
+    type PropsWithRef<P> = P;
+    type ComponentProps<T extends keyof JSX.IntrinsicElements | JSXElementConstructor<any>> =
+        T extends JSXElementConstructor<infer P>
+            ? P
+            : T extends keyof JSX.IntrinsicElements
+                ? JSX.IntrinsicElements[T]
+                : {{}};
+    type ComponentPropsWithRef<T extends ElementType> = PropsWithRef<ComponentProps<T>>;
+}}
+
+declare namespace JSX {{
+    interface IntrinsicElements {{
+        a: {{ onClick?: React.MouseEventHandler<React.AnchorElement> }};
+        button: {{ onClick?: React.MouseEventHandler<React.ButtonElement> }};
+        div: any;
+        span: any;
+    }}
+}}
+
+type Exclude<T, U> = T extends U ? never : T;
+type Pick<T, K extends keyof T> = {{ [P in K]: T[P] }};
+type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+
+declare function UnwrappedLink<T extends React.ElementType = React.ElementType>(
+    props: Omit<React.ComponentPropsWithRef<React.ElementType extends T ? "a" : T>, "as">,
+): null;
+
+declare function UnwrappedLink2<T extends React.ElementType = React.ElementType>(
+    props: Omit<React.ComponentPropsWithRef<React.ElementType extends T ? "a" : T>, "as"> & {{ as?: T }},
+): null;
+
+<UnwrappedLink onClick={{(e) => e.currentTarget.href}} />;
+<UnwrappedLink2 onClick={{(e) => e.currentTarget.href}} />;
+<UnwrappedLink2 as="button" onClick={{(e) => e.currentTarget.disabled}} />;
+"#
+    );
+
+    let diags = jsx_diagnostics(&source);
+    assert!(
+        !has_code(&diags, diagnostic_codes::PARAMETER_IMPLICITLY_HAS_AN_TYPE),
+        "React-style defaulted conditional generic JSX props should contextually type callback attrs, got: {diags:?}"
+    );
+    assert!(
+        !has_code(&diags, diagnostic_codes::TYPE_IS_NOT_ASSIGNABLE_TO_TYPE),
+        "React-style defaulted conditional generic JSX props should keep callback members assignable, got: {diags:?}"
+    );
+}
+
+#[test]
 fn test_generic_props_alias_call_preserves_callback_member() {
     let source = format!(
         r#"
