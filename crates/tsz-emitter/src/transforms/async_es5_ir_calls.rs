5de333bddc21f5ece53644552ee130668aefa074
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-05-24 12:35:30 +0200

    fix(emit): lower async es5 new expressions

--- crates/tsz-emitter/src/transforms/async_es5_ir_calls.rs
+++ crates/tsz-emitter/src/transforms/async_es5_ir_calls.rs
@@ -383,6 +383,490 @@ impl<'a> AsyncES5Transformer<'a> {
         })
     }
 
+    pub(super) fn lower_new_expression_before_suspension(
+        &mut self,
+        idx: NodeIndex,
+        cases: &mut Vec<IRGeneratorCase>,
+        current_statements: &mut Vec<IRNode>,
+        current_label: &mut u32,
+    ) -> Option<IRNode> {
+        let node = self.arena.get(idx)?;
+        if node.kind != syntax_kind_ext::NEW_EXPRESSION {
+            return None;
+        }
+        let new_expr = self.arena.get_call_expr(node)?;
+        if let Some(lowered) = self.lower_new_element_callee_index_before_suspension(
+            new_expr.expression,
+            new_expr
+                .arguments
+                .as_ref()
+                .map(|args| args.nodes.as_slice()),
+            cases,
+            current_statements,
+            current_label,
+        ) {
+            return Some(lowered);
+        }
+        if self.contains_await_recursive(new_expr.expression) {
+            self.emit_nested_suspension(
+                new_expr.expression,
+                cases,
+                current_statements,
+                current_label,
+            );
+            let args = new_expr.arguments.as_ref();
+            if let Some(args) = args
+                && self.args_contain_spread(&args.nodes)
+            {
+                let receiver_temp = self.generate_hoisted_temp();
+                current_statements.push(IRNode::VarDecl {
+                    name: receiver_temp.clone().into(),
+                    initializer: None,
+                });
+                let captured_receiver = IRNode::Parenthesized(Box::new(IRNode::assign(
+                    IRNode::id(receiver_temp.clone()),
+                    IRNode::Parenthesized(Box::new(IRNode::GeneratorSent)),
+                )));
+                let arg_array = self.spread_new_base_array(&args.nodes);
+                return Some(Self::new_from_bound_apply(
+                    IRNode::prop(captured_receiver, "bind"),
+                    IRNode::id(receiver_temp),
+                    arg_array,
+                ));
+            }
+            let args = args
+                .map(|args| {
+                    args.nodes
+                        .iter()
+                        .map(|&arg| self.expression_to_ir(arg))
+                        .collect()
+                })
+                .unwrap_or_default();
+            let callee = self
+                .new_callee_after_suspension(new_expr.expression)
+                .unwrap_or_else(|| IRNode::Parenthesized(Box::new(IRNode::GeneratorSent)));
+            return Some(IRNode::NewExpr {
+                callee: Box::new(callee),
+                arguments: args,
+                explicit_arguments: new_expr.arguments.is_some(),
+            });
+        }
+
+        let args = new_expr.arguments.as_ref()?;
+        let suspension_arg_index = args
+            .nodes
+            .iter()
+            .position(|&arg| self.contains_await_recursive(arg))?;
+        let has_spread = self.args_contain_spread(&args.nodes);
+        let spread_capture = has_spread.then(|| {
+            self.capture_new_bind_apply_before_suspension(new_expr.expression, current_statements)
+        });
+        let bind_capture = (!has_spread).then(|| {
+            self.capture_new_bind_before_suspension(new_expr.expression, current_statements)
+        });
+        let arg_array = self.lower_suspended_new_arguments(
+            &args.nodes,
+            suspension_arg_index,
+            current_statements,
+        );
+
+        self.emit_nested_suspension(
+            args.nodes[suspension_arg_index],
+            cases,
+            current_statements,
+            current_label,
+        );
+
+        if let Some((bind_temp, apply_temp, receiver_args_temp)) = spread_capture {
+            return Some(Self::new_from_apply_apply(
+                IRNode::id(apply_temp),
+                IRNode::id(bind_temp),
+                IRNode::id(receiver_args_temp),
+                arg_array,
+            ));
+        }
+
+        let (bind_temp, receiver) = bind_capture?;
+        Some(Self::new_from_bound_apply(
+            IRNode::id(bind_temp),
+            receiver,
+            arg_array,
+        ))
+    }
+
+    fn lower_new_element_callee_index_before_suspension(
+        &mut self,
+        callee: NodeIndex,
+        args: Option<&[NodeIndex]>,
+        cases: &mut Vec<IRGeneratorCase>,
+        current_statements: &mut Vec<IRNode>,
+        current_label: &mut u32,
+    ) -> Option<IRNode> {
+        let callee_node = self.arena.get(callee)?;
+        if callee_node.kind != syntax_kind_ext::ELEMENT_ACCESS_EXPRESSION {
+            return None;
+        }
+        let access = self.arena.get_access_expr(callee_node)?;
+        if self.contains_await_recursive(access.expression)
+            || !self.contains_await_recursive(access.name_or_argument)
+        {
+            return None;
+        }
+
+        let object_temp = self.generate_hoisted_temp();
+        current_statements.push(IRNode::VarDecl {
+            name: object_temp.clone().into(),
+            initializer: None,
+        });
+        current_statements.push(IRNode::ExpressionStatement(Box::new(IRNode::assign(
+            IRNode::id(object_temp.clone()),
+            self.expression_to_ir(access.expression),
+        ))));
+
+        self.emit_nested_suspension(
+            access.name_or_argument,
+            cases,
+            current_statements,
+            current_label,
+        );
+
+        let arguments = args
+            .map(|args| args.iter().map(|&arg| self.expression_to_ir(arg)).collect())
+            .unwrap_or_default();
+        Some(IRNode::NewExpr {
+            callee: Box::new(IRNode::ElementAccess {
+                object: Box::new(IRNode::id(object_temp)),
+                index: Box::new(IRNode::GeneratorSent),
+            }),
+            arguments,
+            explicit_arguments: args.is_some(),
+        })
+    }
+
+    fn new_callee_after_suspension(&self, callee: NodeIndex) -> Option<IRNode> {
+        let callee_node = self.arena.get(callee)?;
+        if callee_node.kind == syntax_kind_ext::PROPERTY_ACCESS_EXPRESSION {
+            let access = self.arena.get_access_expr(callee_node)?;
+            if self.contains_await_recursive(access.expression)
+                && !self.contains_await_recursive(access.name_or_argument)
+            {
+                let property = crate::transforms::emit_utils::identifier_text_or_empty(
+                    self.arena,
+                    access.name_or_argument,
+                );
+                return Some(IRNode::prop(
+                    IRNode::Parenthesized(Box::new(IRNode::GeneratorSent)),
+                    property,
+                ));
+            }
+        }
+        if callee_node.kind == syntax_kind_ext::ELEMENT_ACCESS_EXPRESSION {
+            let access = self.arena.get_access_expr(callee_node)?;
+            if self.contains_await_recursive(access.expression)
+                && !self.contains_await_recursive(access.name_or_argument)
+            {
+                return Some(IRNode::elem(
+                    IRNode::Parenthesized(Box::new(IRNode::GeneratorSent)),
+                    self.expression_to_ir(access.name_or_argument),
+                ));
+            }
+        }
+        None
+    }
+
+    pub(super) fn lower_es5_new_spread(&mut self, idx: NodeIndex) -> Option<IRNode> {
+        let node = self.arena.get(idx)?;
+        if node.kind != syntax_kind_ext::NEW_EXPRESSION {
+            return None;
+        }
+        let new_expr = self.arena.get_call_expr(node)?;
+        let args = new_expr.arguments.as_ref()?;
+        if !self.args_contain_spread(&args.nodes) {
+            return None;
+        }
+        let callee = self.expression_to_ir(new_expr.expression);
+        let arg_array = self.spread_new_base_array(&args.nodes);
+        Some(Self::new_from_bound_apply(
+            IRNode::prop(callee.clone(), "bind"),
+            callee,
+            arg_array,
+        ))
+    }
+
+    fn capture_new_bind_before_suspension(
+        &mut self,
+        callee: NodeIndex,
+        current_statements: &mut Vec<IRNode>,
+    ) -> (String, IRNode) {
+        let Some(callee_node) = self.arena.get(callee) else {
+            return (String::new(), IRNode::Undefined);
+        };
+        if callee_node.kind == syntax_kind_ext::PROPERTY_ACCESS_EXPRESSION
+            || callee_node.kind == syntax_kind_ext::ELEMENT_ACCESS_EXPRESSION
+        {
+            let constructor_temp = self.generate_hoisted_temp();
+            let bind_temp = self.generate_hoisted_temp();
+            current_statements.push(IRNode::VarDecl {
+                name: constructor_temp.clone().into(),
+                initializer: None,
+            });
+            current_statements.push(IRNode::VarDecl {
+                name: bind_temp.clone().into(),
+                initializer: None,
+            });
+            current_statements.push(IRNode::ExpressionStatement(Box::new(IRNode::assign(
+                IRNode::id(bind_temp.clone()),
+                IRNode::prop(
+                    IRNode::Parenthesized(Box::new(IRNode::assign(
+                        IRNode::id(constructor_temp.clone()),
+                        self.expression_to_ir(callee),
+                    ))),
+                    "bind",
+                ),
+            ))));
+            return (bind_temp, IRNode::id(constructor_temp));
+        }
+
+        let bind_temp = self.generate_hoisted_temp();
+        current_statements.push(IRNode::VarDecl {
+            name: bind_temp.clone().into(),
+            initializer: None,
+        });
+        current_statements.push(IRNode::ExpressionStatement(Box::new(IRNode::assign(
+            IRNode::id(bind_temp.clone()),
+            IRNode::prop(self.expression_to_ir(callee), "bind"),
+        ))));
+        (bind_temp, self.expression_to_ir(callee))
+    }
+
+    fn capture_new_bind_apply_before_suspension(
+        &mut self,
+        callee: NodeIndex,
+        current_statements: &mut Vec<IRNode>,
+    ) -> (String, String, String) {
+        let bind_temp = self.generate_hoisted_temp();
+        let apply_temp = self.generate_hoisted_temp();
+        let receiver_args_temp = self.generate_hoisted_temp();
+        current_statements.push(IRNode::VarDecl {
+            name: bind_temp.clone().into(),
+            initializer: None,
+        });
+        current_statements.push(IRNode::VarDecl {
+            name: apply_temp.clone().into(),
+            initializer: None,
+        });
+        current_statements.push(IRNode::VarDecl {
+            name: receiver_args_temp.clone().into(),
+            initializer: None,
+        });
+        current_statements.push(IRNode::ExpressionStatement(Box::new(IRNode::assign(
+            IRNode::id(apply_temp.clone()),
+            IRNode::prop(
+                IRNode::Parenthesized(Box::new(IRNode::assign(
+                    IRNode::id(bind_temp.clone()),
+                    IRNode::prop(self.expression_to_ir(callee), "bind"),
+                ))),
+                "apply",
+            ),
+        ))));
+        current_statements.push(IRNode::ExpressionStatement(Box::new(IRNode::assign(
+            IRNode::id(receiver_args_temp.clone()),
+            IRNode::ArrayLiteral(vec![self.expression_to_ir(callee)]),
+        ))));
+        (bind_temp, apply_temp, receiver_args_temp)
+    }
+
+    fn lower_suspended_new_arguments(
+        &mut self,
+        args: &[NodeIndex],
+        suspension_arg_index: usize,
+        current_statements: &mut Vec<IRNode>,
+    ) -> IRNode {
+        if self.args_contain_spread(args) {
+            return self.lower_suspended_spread_new_arguments(
+                args,
+                suspension_arg_index,
+                current_statements,
+            );
+        }
+
+        if suspension_arg_index == 0 {
+            let mut lowered_args = Vec::with_capacity(args.len() + 1);
+            lowered_args.push(IRNode::Undefined);
+            lowered_args.extend(args.iter().map(|&arg| self.expression_to_ir(arg)));
+            return IRNode::ArrayLiteral(lowered_args);
+        }
+
+        let prefix_temp = self.generate_hoisted_temp();
+        current_statements.push(IRNode::VarDecl {
+            name: prefix_temp.clone().into(),
+            initializer: None,
+        });
+        let mut prefix_args = Vec::with_capacity(suspension_arg_index + 1);
+        prefix_args.push(IRNode::Undefined);
+        prefix_args.extend(
+            args[..suspension_arg_index]
+                .iter()
+                .map(|&arg| self.expression_to_ir(arg)),
+        );
+        current_statements.push(IRNode::ExpressionStatement(Box::new(IRNode::assign(
+            IRNode::id(prefix_temp.clone()),
+            IRNode::ArrayLiteral(prefix_args),
+        ))));
+
+        let suffix_args = args[suspension_arg_index..]
+            .iter()
+            .map(|&arg| self.expression_to_ir(arg))
+            .collect();
+        IRNode::CallExpr {
+            callee: Box::new(IRNode::prop(IRNode::id(prefix_temp), "concat")),
+            arguments: vec![IRNode::ArrayLiteral(suffix_args)],
+        }
+    }
+
+    fn lower_suspended_spread_new_arguments(
+        &mut self,
+        args: &[NodeIndex],
+        suspension_arg_index: usize,
+        current_statements: &mut Vec<IRNode>,
+    ) -> IRNode {
+        self.helpers_needed.mark_spread_array();
+        let prefix_base = self.spread_new_base_array(&args[..suspension_arg_index]);
+        let suspension_is_spread = self.is_spread_arg(args[suspension_arg_index]);
+        let has_prefix_spread = self.args_contain_spread(&args[..suspension_arg_index]);
+        let has_suffix_spread = self.args_contain_spread(&args[suspension_arg_index + 1..]);
+
+        let mut current = if suspension_is_spread || has_prefix_spread {
+            let prefix_temp = self.generate_hoisted_temp();
+            current_statements.push(IRNode::VarDecl {
+                name: prefix_temp.clone().into(),
+                initializer: None,
+            });
+            current_statements.push(IRNode::ExpressionStatement(Box::new(IRNode::assign(
+                IRNode::id(prefix_temp.clone()),
+                IRNode::ArrayLiteral(vec![prefix_base]),
+            ))));
+            let resumed = if suspension_is_spread {
+                IRNode::Parenthesized(Box::new(IRNode::GeneratorSent))
+            } else {
+                IRNode::ArrayLiteral(vec![IRNode::GeneratorSent])
+            };
+            self.spread_array_apply(IRNode::CallExpr {
+                callee: Box::new(IRNode::prop(IRNode::id(prefix_temp), "concat")),
+                arguments: vec![IRNode::ArrayLiteral(vec![
+                    resumed,
+                    IRNode::BooleanLiteral(false),
+                ])],
+            })
+        } else if has_suffix_spread {
+            let prefix_temp = self.generate_hoisted_temp();
+            current_statements.push(IRNode::VarDecl {
+                name: prefix_temp.clone().into(),
+                initializer: None,
+            });
+            current_statements.push(IRNode::ExpressionStatement(Box::new(IRNode::assign(
+                IRNode::id(prefix_temp.clone()),
+                prefix_base,
+            ))));
+            IRNode::CallExpr {
+                callee: Box::new(IRNode::prop(IRNode::id(prefix_temp), "concat")),
+                arguments: vec![IRNode::ArrayLiteral(vec![IRNode::GeneratorSent])],
+            }
+        } else {
+            IRNode::ArrayLiteral(vec![IRNode::Undefined, IRNode::GeneratorSent])
+        };
+
+        let mut segment = Vec::new();
+        for &arg in &args[suspension_arg_index + 1..] {
+            if self.is_spread_arg(arg) {
+                if !segment.is_empty() {
+                    current = self.spread_array_apply(IRNode::ArrayLiteral(vec![
+                        current,
+                        IRNode::ArrayLiteral(std::mem::take(&mut segment)),
+                        IRNode::BooleanLiteral(false),
+                    ]));
+                }
+                current = self.spread_array_apply(IRNode::ArrayLiteral(vec![
+                    current,
+                    self.spread_arg_expression_to_ir(arg),
+                    IRNode::BooleanLiteral(false),
+                ]));
+            } else {
+                segment.push(self.expression_to_ir(arg));
+            }
+        }
+        if !segment.is_empty() {
+            current = self.spread_array_apply(IRNode::ArrayLiteral(vec![
+                current,
+                IRNode::ArrayLiteral(segment),
+                IRNode::BooleanLiteral(false),
+            ]));
+        }
+        current
+    }
+
+    fn spread_new_base_array(&mut self, args: &[NodeIndex]) -> IRNode {
+        if !self.args_contain_spread(args) {
+            let mut lowered = Vec::with_capacity(args.len() + 1);
+            lowered.push(IRNode::Undefined);
+            lowered.extend(args.iter().map(|&arg| self.expression_to_ir(arg)));
+            return IRNode::ArrayLiteral(lowered);
+        }
+
+        let mut current = IRNode::ArrayLiteral(vec![IRNode::Undefined]);
+        let mut segment = Vec::new();
+        for &arg in args {
+            if self.is_spread_arg(arg) {
+                if !segment.is_empty() {
+                    current = self.spread_array_call(
+                        current,
+                        IRNode::ArrayLiteral(std::mem::take(&mut segment)),
+                    );
+                }
+                current = self.spread_array_call(current, self.spread_arg_expression_to_ir(arg));
+            } else {
+                segment.push(self.expression_to_ir(arg));
+            }
+        }
+        if !segment.is_empty() {
+            current = self.spread_array_call(current, IRNode::ArrayLiteral(segment));
+        }
+        current
+    }
+
+    fn new_from_bound_apply(bind: IRNode, receiver: IRNode, arg_array: IRNode) -> IRNode {
+        IRNode::NewExpr {
+            callee: Box::new(IRNode::Parenthesized(Box::new(IRNode::CallExpr {
+                callee: Box::new(IRNode::prop(bind, "apply")),
+                arguments: vec![receiver, arg_array],
+            }))),
+            arguments: Vec::new(),
+            explicit_arguments: true,
+        }
+    }
+
+    fn new_from_apply_apply(
+        apply_method: IRNode,
+        bind_function: IRNode,
+        receiver_args: IRNode,
+        arg_array: IRNode,
+    ) -> IRNode {
+        IRNode::NewExpr {
+            callee: Box::new(IRNode::Parenthesized(Box::new(IRNode::CallExpr {
+                callee: Box::new(IRNode::prop(apply_method, "apply")),
+                arguments: vec![
+                    bind_function,
+                    IRNode::CallExpr {
+                        callee: Box::new(IRNode::prop(receiver_args, "concat")),
+                        arguments: vec![IRNode::ArrayLiteral(vec![arg_array])],
+                    },
+                ],
+            }))),
+            arguments: Vec::new(),
+            explicit_arguments: true,
+        }
+    }
+
     fn args_contain_spread(&self, args: &[NodeIndex]) -> bool {
         args.iter().any(|&arg| self.is_spread_arg(arg))
     }
