b07919d7a739aa3686a5ecf379095743d186509b
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-06-03 08:50:59 +0200

    refactor(checker): split cross-file miss classifier

--- crates/tsz-checker/src/state/type_analysis/cross_file.rs
+++ crates/tsz-checker/src/state/type_analysis/cross_file.rs
@@ -126,51 +126,6 @@ impl<'a> CheckerState<'a> {
         self.get_symbol_globally(sym_id)
     }
 
-    fn cross_arena_symbol_miss_kind(&self, sym_id: SymbolId) -> CrossArenaSymbolMissKind {
-        let Some(flags) = self
-            .get_cross_file_symbol(sym_id)
-            .map(|symbol| symbol.flags)
-        else {
-            return CrossArenaSymbolMissKind::Unresolved;
-        };
-
-        if flags & symbol_flags::TYPE_ALIAS != 0 {
-            CrossArenaSymbolMissKind::TypeAlias
-        } else if flags & symbol_flags::INTERFACE != 0 {
-            CrossArenaSymbolMissKind::Interface
-        } else if flags & symbol_flags::CLASS != 0 {
-            CrossArenaSymbolMissKind::Class
-        } else if flags & symbol_flags::FUNCTION != 0 {
-            CrossArenaSymbolMissKind::Function
-        } else if flags & symbol_flags::VARIABLE != 0 {
-            CrossArenaSymbolMissKind::Variable
-        } else if flags & symbol_flags::PROPERTY != 0 {
-            CrossArenaSymbolMissKind::Property
-        } else if flags & symbol_flags::METHOD != 0 {
-            CrossArenaSymbolMissKind::Method
-        } else if flags & symbol_flags::ACCESSOR != 0 {
-            CrossArenaSymbolMissKind::Accessor
-        } else if flags & symbol_flags::ENUM != 0 {
-            CrossArenaSymbolMissKind::Enum
-        } else if flags & symbol_flags::MODULE != 0 {
-            CrossArenaSymbolMissKind::Module
-        } else if flags & symbol_flags::ALIAS != 0 {
-            CrossArenaSymbolMissKind::Alias
-        } else if flags & symbol_flags::TYPE_PARAMETER != 0 {
-            CrossArenaSymbolMissKind::TypeParameter
-        } else if flags & symbol_flags::TYPE_LITERAL != 0 {
-            CrossArenaSymbolMissKind::TypeLiteral
-        } else if flags & symbol_flags::SIGNATURE != 0 {
-            CrossArenaSymbolMissKind::Signature
-        } else if flags & symbol_flags::CONSTRUCTOR != 0 {
-            CrossArenaSymbolMissKind::Constructor
-        } else if flags & symbol_flags::OBJECT_LITERAL != 0 {
-            CrossArenaSymbolMissKind::ObjectLiteral
-        } else {
-            CrossArenaSymbolMissKind::Other
-        }
-    }
-
     fn try_resolve_cross_arena_named_alias_without_child(
         &mut self,
         sym_id: SymbolId,
@@ -2013,6 +1968,8 @@ impl<'a> CheckerState<'a> {
         derived_type
     }
 }
+include!("cross_file_miss_kind.rs");
+
 #[cfg(test)]
 #[path = "cross_file_query_kind_tests.rs"]
 mod cross_file_query_kind_tests;
